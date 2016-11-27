1ff454743e33a3d529ce1bf912feccd1cbd17551
kristinadarroch <dar15003@byui.edu>
2016-11-27 01:03:12 -0700

    weather

--- CIT230 Final Project/jquery.js
+++ CIT230 Final Project/jquery.js
@@ -1,6 +1,6 @@
 // a jQuery function to implement modularization
 $(function() {
-    console.log("test");
+    console.log();
     $("#page-header-kk").load("page-header-kk.html");
     $("#page-nav-kk").load("page-nav-kk.html");
     $("#page-footer-kk").load("page-footer-kk.html");
