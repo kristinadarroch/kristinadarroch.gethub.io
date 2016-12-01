77f5788dde4ffd72134ce2e189ff979e112945b6
kristinadarroch <dar15003@byui.edu>
2016-11-30 17:54:11 -0700

    save

--- menu-activity/scripts/search.js
+++ menu-activity/scripts/search.js
@@ -2,7 +2,7 @@ $('#query').keyup(function(){
   // my code goes here
     var value = $('#query').val();
     var rExp = new RegExp(value, "i");
-    $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
   
   }); // end getJSON
     
