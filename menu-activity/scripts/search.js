af4b44dd7401a104b43da38dec27be78b3402569
kristinadarroch <dar15003@byui.edu>
2016-11-30 17:58:46 -0700

    new

--- menu-activity/scripts/search.js
+++ menu-activity/scripts/search.js
@@ -1,11 +1,21 @@
-$('#query').keyup(function(){
-  // my code goes here
-    var value = $('#query').val();
-    var rExp = new RegExp(value, "i");
-    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
-  
+$('#query').keyup(function() {
+  var value = $('#query').val();
+  var rExp = new RegExp(value, "i");
+  $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
+    console.log(data); // test for JSON received
+    // Begin building output
+    var output = '<ol>';
+    $.each(data.RESULTS, function(key, val) {
+      if (val.name.search(rExp) != -1) {
+        output += '<li>';
+        output += '<a href="http://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '</li>';
+      }
+    }); // end each
+    output += '</ol>';
+    $("#searchResults").html(output); // send results to the page
   }); // end getJSON
+}); // end onkeyup
     
-    console.log(data);
-    
-}); // end keyup
+
+
